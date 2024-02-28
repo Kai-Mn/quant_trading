7ddc1a95f3dab62bbfa54eb373382827f19606c1
Kai <114399435+Kai-Mn@users.noreply.github.com>
2024-02-28 16:32:34 +0100

    implements broken celery

--- quant_trading/quant_trading/settings.py
+++ quant_trading/quant_trading/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery Configuration Options
+CELERY_TIMEZONE = "Australia/Tasmania"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
