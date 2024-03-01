d6067aa42a877269978fb0ecff4ce126356891d0
Kai <114399435+Kai-Mn@users.noreply.github.com>
2024-03-01 19:23:54 +0100

    implements stock and companies tables and a way to fetch data into them from yfinance

--- quant_trading/frontend/tasks.py
+++ quant_trading/frontend/tasks.py
@@ -1,9 +1,22 @@
 # from quant_trading import celery_app
 import yfinance as yf
-# data = yf.download("AAPL", start="2020-01-01", end="2021-01-01")
+from django.conf import settings
+from frontend.models import Stocks
+from sqlalchemy import create_engine
 
-# @celery_app.task
-# def fetch_data()
-# data = yf.download("AAPL", period="5d")
-data = yf.Ticker("MSFT")
-print(data.insider_transactions)
+user = settings.DATABASES['default']['USER']
+password = settings.DATABASES['default']['PASSWORD']
+database_name = settings.DATABASES['default']['NAME']
+host = settings.DATABASES['default']['HOST']
+port = settings.DATABASES['default']['PORT']
+
+database_url = 'mariadb://{user}:{password}@db:3306/{database_name}'.format(
+    user=user,
+    password=password,
+    database_name=database_name,
+)
+
+engine = create_engine(database_url, echo=False)
+
+data = yf.download("AAPL", period="5d")
+data.to_sql('frontend_stocks',engine,if_exists='replace')
